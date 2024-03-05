fd5fb094b0235138c4cfb162012b389691e2856d
danylosus <danil.sushko19@gmail.com>
2024-03-05 20:16:03 +0200

    Main Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,49 @@
 import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 
 void main() {
-  runApp(const MaterialApp(home: Text("sis")));
+  runApp(const Content());
+}
+
+class Content extends StatelessWidget {
+  const Content({super.key});
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.purple, Colors.purpleAccent],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Wrap(
+                  direction: Axis.vertical,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  spacing: 16,
+                  children: [
+                    Image.asset(
+                      "assets/images/quiz-logo.png",
+                      width: 300,
+                    ),
+                    const Text(
+                      "Quiz App",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => {},
+                      child: const Text("Start Quiz"),
+                    )
+                  ]),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
 }
